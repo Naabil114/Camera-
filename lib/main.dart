fe2c5ef45808fb01455e2c4b6f08eb445b7b16b0
nabil <nabiltrobel.23@gmail.com>
2025-11-13 11:14:51 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'ambil_gambar.dart';
+import 'ambil_video.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "Multimedia",
+            ),
+            bottom: TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.queue_music),
+                  text: "Ambil Gambar",
+                ),
+                Tab(
+                  icon: Icon(Icons.video_library),
+                  text: "Ambil Video",
+                ),
+              ],
+              indicatorColor: Colors.amber,
+            ),
+          ),
+          body: Container(
+            height: double.infinity,
+            width: double.infinity,
+            child: TabBarView(
+              children: [
+                AmbilGambar(),
+                AmbilVideo(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
